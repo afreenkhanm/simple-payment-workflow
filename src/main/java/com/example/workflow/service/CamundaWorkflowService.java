1ef8b62b6eb7c042a6c8480d5b4ebabeb47d438f
alohia <alohia@AIPL-PUN-LT201.altimetrik.com>
2020-01-06 17:28:06 +0530

    formatted code

--- src/main/java/com/example/workflow/service/CamundaWorkflowService.java
+++ src/main/java/com/example/workflow/service/CamundaWorkflowService.java
@@ -1,16 +1,7 @@
 package com.example.workflow.service;
 
 import com.example.workflow.dto.PaymentDto;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public interface CamundaWorkflowService {
-
-    String initWorkflow(PaymentDto newPayment);
-
+	String initWorkflow(PaymentDto newPayment);
 }
--- src/main/java/com/example/workflow/service/impl/CamundaWorkflowServiceImpl.java
+++ src/main/java/com/example/workflow/service/impl/CamundaWorkflowServiceImpl.java
@@ -1,32 +1,31 @@
 package com.example.workflow.service.impl;
 
-import com.example.workflow.dto.PaymentDto;
-import com.example.workflow.service.CamundaWorkflowService;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.example.workflow.dto.PaymentDto;
+import com.example.workflow.service.CamundaWorkflowService;
 
 @Service
 public class CamundaWorkflowServiceImpl implements CamundaWorkflowService {
 
-    @Autowired
-    private RuntimeService runtimeService;
+	private RuntimeService runtimeService;
 
-    @Override
-    public String initWorkflow(PaymentDto newPayment) {
+	@Autowired
+	public CamundaWorkflowServiceImpl(RuntimeService runtimeService) {
+		this.runtimeService = runtimeService;
+	}
 
-        Map<String, Object> workflowVariables = new HashMap<>();
-        workflowVariables.put("amount", newPayment.getAmount());
-        workflowVariables.put("txId", newPayment.getTxId());
-
-        ProcessInstanceWithVariables processInstanceWithVariables = runtimeService.createProcessInstanceByKey("payment-application")
-                .setVariables(workflowVariables).executeWithVariablesInReturn();
-
-        return processInstanceWithVariables.getId();
-
-    }
+	@Override
+	public String initWorkflow(PaymentDto newPayment) {
+		Map<String, Object> workflowVariables = new HashMap<>();
+		workflowVariables.put("amount", newPayment.getAmount());
+		workflowVariables.put("txId", newPayment.getTxId());
+		return runtimeService.createProcessInstanceByKey("payment-application").setVariables(workflowVariables)
+				.executeWithVariablesInReturn().getId();
+	}
 }
