0a20f0b44c59f6aeab69d67bfa00d4675c8ee459
alohia <alohia@AIPL-PUN-LT201.altimetrik.com>
2020-01-06 17:28:53 +0530

    minor

--- src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
@@ -1,13 +1,13 @@
 package com.example.workflow.service.impl;
 
-import com.example.workflow.service.CamundaWorkflowService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.workflow.dto.GenericResponseDto;
 import com.example.workflow.dto.PaymentDto;
 import com.example.workflow.repository.PaymentRepository;
+import com.example.workflow.service.CamundaWorkflowService;
 import com.example.workflow.service.PaymentService;
-import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
