eb1631b4b03d39c5b9dabda9fddf0ea93d889810
akhanum <afreen.khanum124@gmail.com>
2020-01-06 15:08:34 +0530

    service registration

--- src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
@@ -6,7 +6,9 @@ import com.example.workflow.dto.GenericResponseDto;
 import com.example.workflow.dto.PaymentDto;
 import com.example.workflow.repository.PaymentRepository;
 import com.example.workflow.service.PaymentService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 	
 	private PaymentRepository paymentRepository;
