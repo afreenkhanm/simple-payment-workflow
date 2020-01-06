c5cdab65282db5d436eb7b4db4e5370a2c782902
alohia <alohia@AIPL-PUN-LT201.altimetrik.com>
2020-01-06 12:19:04 +0530

    added entity and repo

---
+++ src/main/java/com/example/workflow/entity/Payment.java
@@ -0,0 +1,23 @@
+package com.example.workflow.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String txId;
+	private BigDecimal amount = BigDecimal.ZERO;
+	private boolean approved;
+}
--- src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/workflow/service/impl/PaymentServiceImpl.java
@@ -1,10 +1,20 @@
 package com.example.workflow.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.workflow.dto.GenericResponseDto;
 import com.example.workflow.dto.NewPayment;
+import com.example.workflow.repository.PaymentRepository;
 import com.example.workflow.service.PaymentService;
 
 public class PaymentServiceImpl implements PaymentService {
+	
+	private PaymentRepository paymentRepository;
+
+	@Autowired
+	public PaymentServiceImpl(PaymentRepository paymentRepository) {
+		this.paymentRepository = paymentRepository;
+	}
 
 	@Override
 	public GenericResponseDto newPayment(NewPayment payment) {
